cc582065e9cb9301403eacdf4acc310ce71578e5
Tamás Krizsán <tamas@krizsanmunka.hu>
2020-10-27 23:17:10 +0100

    feat(function-library): add "rhash.sh" shared script file

---
+++ function-library/rhash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# RHash shared settings.
+# Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### INCLUDES
+
+source bash.inc.sh
+
+
+### SHARED SETTINGS
+
+## Constants
+
+readonly -- RHASH_SHARED_SETTINGS='--verbose --speed --percents';
