2ac5dd1f94b0bb070a29e1e504da6908b2d14904
Tamás Krizsán <tamas@krizsanmunka.hu>
2020-10-28 09:36:55 +0100

    feat(function-call): add "call_read_hash.sh" function call script

---
+++ function-call/call_read_hash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Call read_hash function.
+# Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### INCLUDES
+
+source read_hash.sh
+
+
+### INPUT PARAMETERS
+
+input="";
+
+
+### FUNCTION CALL
+
+read_hash "$input";
+
+next_script "" "";
