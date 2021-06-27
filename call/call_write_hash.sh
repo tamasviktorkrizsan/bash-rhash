3128de7d2314649464a7b51f8db2e1a6c9ec9413
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-06-27 10:42:17 +0200

    feature: add "call_write_hash" function caller

---
+++ call/call_write_hash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Call write_hash function.
+# Copyright (C) 2021 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### INPUT PARAMETERS
+
+input="";
+
+
+### INCLUDES
+
+source write_hash.sh
+
+
+### CALL FUNCTION
+
+write_hash "$input";
+
+next_script "" "";
