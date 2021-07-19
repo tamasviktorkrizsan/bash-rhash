301714c00e85be5df2fb3e5ef0f450c67f2cb82d
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-07-19 16:24:20 +0200

    refactor: change function default value behaviour

--- install/lib/write_hash.sh
+++ install/lib/write_hash.sh
@@ -26,28 +26,44 @@ declare usr_input=$1;
 
 ### PROCESSING
 
-# If there is no input paramteter has given, then hash all files/subfolders in the root.
-if [[ -z "${usr_input}" ]]
-  then usr_input=$(basename $PWD);
+declare input_name;
 
-  # Step back in the folder tree.
 
-  cd ..;
+if [[ -n "${usr_input}" ]]
+
+  then input_name=$usr_input;
+
+  # If there is no input paramteter has given, then hash all files/subfolders in the root.
+  # get root folder name
+
+  else input_name=$(basename "$PWD");
+
+  # create checksum folder and enter it.
+
+  declare hash_folder="Z_VERSION_CONTROL\CHECKSUM";
+
+  make_dir "$hash_folder";
+
+  cd "$hash_folder";
+
+
+  # hash all files/subfolders in the root
+
+  usr_input="../../";
 
 fi
 
+declare output_filename;
 
-declare output;
+output_filename=$(replace_whitespace "$input_name");
 
-output=$(replace_whitespace "$usr_input");
-
-declare cfg_write="--recursive --follow --sha256 --exclude=.sha256 --printf=%{sha-256}\t%p\n --update=${output%.*}.sha256";
+declare cfg_write="--recursive --follow --sha256 --exclude=.sha256 --printf=%{sha-256}\t%p\n --update=${output_filename%.*}.sha256";
 
 
 # EXECUTING COMMAND
 
 rhash $RHASH_SHARED_SETTINGS $cfg_write "$usr_input" 2>&1 |\
 
-tee "$output.log";
+tee "$output_filename.log";
 
 }
